39ec72bdfea61539715d1572912726b90a1e3e6b
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-01-27 01:01:45 +0700

    Install webfontloader dependencies and add some google font-families

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import App from './App.vue'
 // Stylesheets
 import './assets/styles/entry.scss'
 
+// Webfontloader
+const webfont = require('webfontloader')
+webfont.load({
+  google: {
+    families: ['Open Sans', 'Montserrat', 'Source Sans Pro', 'Raleway', 'Poppins']
+  }
+})
+
 // Octicons
 const octicons = require('@primer/octicons')
 Vue.prototype.$octicons = octicons
